51c67049585be304eee315bab33c38f9e66445e1
Allex Rodrigues <allexxrodriguess@gmail.com>
2022-03-21 22:11:30 -0300

    Fix MS to Steam conversion + QOL improvements

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,14 +29,42 @@ namespace NieR_Automata_Save_Converter
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.MicrosoftSaveFileName = new System.Windows.Forms.TextBox();
             this.LoadOriginalSave = new System.Windows.Forms.Button();
             this.LoadNewSave = new System.Windows.Forms.Button();
             this.ConvertSavegameButton = new System.Windows.Forms.Button();
-            this.MicrosoftSaveFileName = new System.Windows.Forms.Label();
-            this.SteamSaveName = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.SteamSaveName = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(9, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "by Github.com/BelovedTomboyishGirl";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Convert Microsoft Store to Steam";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ConvertToSteam_Click);
+            // 
+            // MicrosoftSaveFileName
+            // 
+            this.MicrosoftSaveFileName.Location = new System.Drawing.Point(180, 14);
+            this.MicrosoftSaveFileName.Name = "MicrosoftSaveFileName";
+            this.MicrosoftSaveFileName.ReadOnly = true;
+            this.MicrosoftSaveFileName.Size = new System.Drawing.Size(241, 20);
+            this.MicrosoftSaveFileName.TabIndex = 8;
             // 
             // LoadOriginalSave
             // 
@@ -68,71 +96,42 @@ namespace NieR_Automata_Save_Converter
             this.ConvertSavegameButton.UseVisualStyleBackColor = true;
             this.ConvertSavegameButton.Click += new System.EventHandler(this.ConvertButton_Click);
             // 
-            // MicrosoftSaveFileName
-            // 
-            this.MicrosoftSaveFileName.AutoSize = true;
-            this.MicrosoftSaveFileName.Location = new System.Drawing.Point(180, 17);
-            this.MicrosoftSaveFileName.Name = "MicrosoftSaveFileName";
-            this.MicrosoftSaveFileName.Size = new System.Drawing.Size(0, 13);
-            this.MicrosoftSaveFileName.TabIndex = 4;
-            // 
             // SteamSaveName
             // 
-            this.SteamSaveName.AutoSize = true;
-            this.SteamSaveName.Location = new System.Drawing.Point(180, 49);
+            this.SteamSaveName.Location = new System.Drawing.Point(180, 46);
             this.SteamSaveName.Name = "SteamSaveName";
-            this.SteamSaveName.Size = new System.Drawing.Size(0, 13);
-            this.SteamSaveName.TabIndex = 5;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label1.Location = new System.Drawing.Point(9, 123);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(185, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "by Github.com/BelovedTomboyishGirl";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 97);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(182, 23);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Convert Microsoft Store to Steam";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.ConvertToSteam_Click);
+            this.SteamSaveName.ReadOnly = true;
+            this.SteamSaveName.Size = new System.Drawing.Size(241, 20);
+            this.SteamSaveName.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(876, 154);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(433, 120);
             this.Controls.Add(this.SteamSaveName);
             this.Controls.Add(this.MicrosoftSaveFileName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.ConvertSavegameButton);
             this.Controls.Add(this.LoadNewSave);
             this.Controls.Add(this.LoadOriginalSave);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox MicrosoftSaveFileName;
         private System.Windows.Forms.Button LoadOriginalSave;
         private System.Windows.Forms.Button LoadNewSave;
         private System.Windows.Forms.Button ConvertSavegameButton;
-        private System.Windows.Forms.Label MicrosoftSaveFileName;
-        private System.Windows.Forms.Label SteamSaveName;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox SteamSaveName;
     }
 }
 
